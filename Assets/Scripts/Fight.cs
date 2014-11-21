58f238dc470589d77c393c015b1f3ce3d87b9e65
TomSmith27 <tom.smith2711@googlemail.com>
2014-11-21 00:09:33 +0000

    Initial Commit Of NumberKnight
    The first commit of numberknight on 21/11/2014

---
+++ Assets/Scripts/Fight.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Fight : MonoBehaviour {
+
+    public GameObject self;
+    public GameObject opponent;
+    public Vector3 initialPos;
+	public enum GameState
+	{
+		Fighting
+
+	};
+    public bool Dead
+    {
+        get;
+        set;
+    }
+	public GameState CurrentGameState {
+				get;
+				set;
+	}
+    public bool Attacking
+    {
+        get;
+        set;
+    }
+    public HealthBar HealthBar
+    {
+        get;
+        set;
+    }
+    public bool BackAtSpawn
+    {
+        get;
+        set;
+    }
+
+	// Use this for initialization
+	public void Start () {
+        initialPos = this.transform.position;
+        HealthBar = this.GetComponent<HealthBar>();
+
+	}
+    public void FindOpponent(string opponentTag)
+    {
+        var potentialOpponents = GameObject.FindGameObjectsWithTag(opponentTag);
+
+        foreach (var opp in potentialOpponents)
+        {
+            if (!opp.GetComponent<Fight>().Dead)
+            {
+                opponent = opp;
+                return;
+            }
+        }
+    }
+    void GetBackToInitalPos()
+    {
+        if (Vector3.Distance(this.transform.position, initialPos) > 0.25 && !BackAtSpawn)
+        {
+            float step = 10 * Time.deltaTime;
+            this.transform.position = (Vector3.MoveTowards(this.transform.position, initialPos, step));
+            if (Vector3.Distance(this.transform.position, initialPos) < 0.25)
+            {
+                BackAtSpawn = true;
+                this.GetComponent<Animator>().SetBool("Moving", false);
+            }
+        }
+    }
+    public void DamageOpponent()
+    {
+            Attacking = false;
+            opponent.GetComponent<HealthBar>().Health--;
+            this.GetComponent<Animator>().SetTrigger("Attack");
+            GameObject.Find("GUI").GetComponent<QuestionRenderer>().GenerateNewQuestion();
+            this.GetComponent<AudioSource>().Play();
+            BackAtSpawn = false;
+    }
+    public void StartAttack()
+    {
+        Attacking = true;
+    }
+    bool MoveToOpponent()
+    {
+        float step = 10 * Time.deltaTime;
+        this.transform.position = (Vector3.MoveTowards(this.transform.position, opponent.transform.position, step));
+        return (Vector3.Distance(this.transform.position, opponent.transform.position) < 1);
+        
+    }
+	// Update is called once per frame
+	public void Update () {
+	 	switch(CurrentGameState)
+		{
+			case GameState.Fighting:
+			{
+		        if (Attacking)
+				{ if (MoveToOpponent())
+		                DamageOpponent();
+				}
+		        else if(!Dead)
+		            GetBackToInitalPos();
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class SceneManager {
+
+    public static void ChangeScene(int sceneIndex)
+    {
+        Application.LoadLevel(sceneIndex);
+    }
+}
